90d1987f273d739c36c26a33cd7820c4c5018670
Prosperity <prosperauthor@gmail.com>
2020-12-27 17:01:18 +0100

    add procfile

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './App';
 import Web3 from 'web3';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+var PORT = process.env.PORT || 3000
 
 ReactDOM.render(<App />, document.getElementById('root'));
